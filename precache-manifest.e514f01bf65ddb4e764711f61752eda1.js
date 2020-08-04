913995ac522eefb017f56655757d3037114fc604
Muhammad Mahmudun Nabi <mmnabi.cse@gmail.com>
2020-08-04 17:04:30 +0600

    Updates

---
+++ precache-manifest.e514f01bf65ddb4e764711f61752eda1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cef41ef2556a5d40c8291057881ab583",
+    "url": "/robodox/index.html"
+  },
+  {
+    "revision": "3c8957f70d9c0f6f02f9",
+    "url": "/robodox/static/css/main.632a964f.chunk.css"
+  },
+  {
+    "revision": "e1b28d8e1cae6120e18b",
+    "url": "/robodox/static/js/2.31182915.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robodox/static/js/2.31182915.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c8957f70d9c0f6f02f9",
+    "url": "/robodox/static/js/main.2e2c5769.chunk.js"
+  },
+  {
+    "revision": "b076dcef4d297b69276a",
+    "url": "/robodox/static/js/runtime-main.18518254.js"
+  }
+]);
